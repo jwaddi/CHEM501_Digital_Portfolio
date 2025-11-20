948f91c339c9782d55792faf77df8bad3ef87057
Josh Waddington <sgjwaddi@liverpool.ac.uk>
2025-11-20 13:05:55 +0000

    Update Nicla logic: Add Serial debug and 1s delay
    Initialised Serial at 115200 baud, added wait for connection in setup, and introduced a 1000ms delay in the main loop.

--- NiclaSenseME_BHY2/NiclaSenseME_BHY2.ino
+++ NiclaSenseME_BHY2/NiclaSenseME_BHY2.ino
@@ -2,32 +2,47 @@
 
 SensorBSEC bsec(SENSOR_ID_BSEC);
 
-unsigned long lastPrint = 0;
-const int printInterval = 1000;
-
 void setup() {
   Serial.begin(115200);
-  BHY2Host.begin();
+  while(!Serial); // WAIT here until Serial Monitor is opened.
+
+  Serial.println("DEBUG: Serial connected. Initializing BHY2 Host...");
+
+  // If connection fails, it might hang here.
+  if (BHY2Host.begin(false)) { // 'false' means don't wait forever
+      Serial.println("DEBUG: Nicla found and connected!");
+  } else {
+      Serial.println("ERROR: Failed to find Nicla! Check black ESLOV cable.");
+      while(1); // Stop here forever if failed
+  }
+
+  Serial.println("DEBUG: Initializing BSEC sensor...");
   bsec.begin();
-  // Updated header to include Accuracy
+  Serial.println("DEBUG: BSEC started. Waiting for first data...");
   Serial.println("Timestamp(ms),Temperature(C),Humidity(%),CO2(ppm),IAQ,Accuracy");
 }
 
 void loop() {
   BHY2Host.update();
-
-  if (millis() - lastPrint >= printInterval) {
+ 
+  static unsigned long lastPrint = 0;
+  // Print every 1 second
+  if (millis() - lastPrint >= 1000) {
     lastPrint = millis();
-    Serial.print(millis());
-    Serial.print(",");
-    Serial.print(bsec.comp_t());
-    Serial.print(",");
-    Serial.print(bsec.comp_h());
-    Serial.print(",");
-    Serial.print(bsec.co2_eq());
-    Serial.print(",");
-    Serial.print(bsec.iaq());
-    Serial.print(",");
-    Serial.println(bsec.accuracy()); // 0 = Warming up, 3 = Best
+   
+    // Only print if the sensor actually has valid data to send
+    // if (bsec.accuracy() >= 0) {
+        Serial.print(millis());
+        Serial.print(",");
+        Serial.print(bsec.comp_t());
+        Serial.print(",");
+        Serial.print(bsec.comp_h());
+        Serial.print(",");
+        Serial.print(bsec.co2_eq());
+        Serial.print(",");
+        Serial.print(bsec.iaq());
+        Serial.print(",");
+        Serial.println(bsec.accuracy());
+    // }
   }
 }
